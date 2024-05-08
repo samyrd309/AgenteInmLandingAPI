f1eb511864c20ebbc533fd1df524d5ee6fcc41ab
Samuel Charles <samyrd309@gmail.com>
2024-05-08 00:14:33 -0400

    propiedades EF Core Configuration By COnvention

--- Models/Propiedad.cs
+++ Models/Propiedad.cs
@@ -1,9 +1,13 @@
 global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
 
+[Table("Propiedades")]
 public class Propiedad{
+    [Key]
+    [Column("IdPropiedad")]
     public int Id { get; set; }
     [Required]
-    public string Direccion{ get; set; }
+    public string ? Direccion{ get; set; }
     [Required]
     public string ? Ciudad{ get; set; }
     [Required]
@@ -11,12 +15,23 @@ public class Propiedad{
     [Required] 
     public string ? Pais{ get; set; }
     [Required]
-    public decimal MetrosCuadrados{ get; set; } = 0;
-    public decimal Precio{ get; set; } = 0;
-    public int NumHabitaciones{ get; set; } = 0;
-    public int NumBanos{ get; set; } = 0; 
-    public int numParqueaderos{ get; set; } = 0;
+    public decimal MetrosCuadrados{ get; set; } 
+    public decimal Precio{ get; set; }
+    public int NumHabitaciones{ get; set; } 
+    public int NumBanos{ get; set; } 
+    public int NumParqueaderos{ get; set; } 
     public string? Descripcion{ get; set; }
-    public bool Disponible { get; set; } = true;
+    public bool Disponible { get; set; }
+
+    public Propiedad(){
+        MetrosCuadrados = 0;
+        Precio = 0;
+        NumHabitaciones = 0;
+        NumBanos = 0;
+        NumParqueaderos = 0;
+        Disponible = true;
+
+    }
+
 
 }
