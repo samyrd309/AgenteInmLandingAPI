78c47fb70c90a5c4a24f214c21afd9169b6edbd5
Samuel Charles <samyrd309@gmail.com>
2024-05-08 09:41:50 -0400

    Adding namespace and creating constructor

--- Entities/Models/Visita.cs
+++ Entities/Models/Visita.cs
@@ -1,30 +1,41 @@
-[Table("Visitas")]
-public class Visita
+namespace Entities.Models
 {
-    [Key]
-    [Column("IdVisita")]
-    public int Id { get; set; }
-    [Required]
-    public DateTime FechaHora { get; set; }
-    [Required]
-    // Clave foránea para AgenteInmobiliario
-    [ForeignKey(nameof(AgenteInmobiliario))]
-    public int AgenteInmobiliarioId { get; set; }
-    [Required]
-    // Propiedad de navegación para AgenteInmobiliario
-    public AgenteInmobiliario AgenteInmobiliario { get; set; }
-    [Required]
-    // Clave foránea para Cliente
-    [ForeignKey(nameof(Cliente))]
-    public int ClienteId { get; set; }
-    [Required]
-    // Propiedad de navegación para Cliente
-    public Cliente Cliente { get; set; }
+    [Table("Visitas")]
+    public class Visita
+    {
+        [Key]
+        [Column("IdVisita")]
+        public int Id { get; set; }
+        [Required]
+        public DateTime FechaHora { get; set; }
+        [Required]
+        // Clave foránea para AgenteInmobiliario
+        [ForeignKey(nameof(AgenteInmobiliario))]
+        public int AgenteInmobiliarioId { get; set; }
+        [Required]
+        // Propiedad de navegación para AgenteInmobiliario
+        public AgenteInmobiliario AgenteInmobiliario { get; set; }
+        [Required]
+        // Clave foránea para Cliente
+        [ForeignKey(nameof(Cliente))]
+        public int ClienteId { get; set; }
+        [Required]
+        // Propiedad de navegación para Cliente
+        public Cliente Cliente { get; set; }
 
-    // Clave foránea para Propiedad
-    [ForeignKey(nameof(Propiedad))]
-    public int PropiedadId { get; set; }
+        // Clave foránea para Propiedad
+        [ForeignKey(nameof(Propiedad))]
+        public int PropiedadId { get; set; }
 
-    // Propiedad de navegación para Propiedad
-    public Propiedad Propiedad { get; set; }
+        // Propiedad de navegación para Propiedad
+        public Propiedad Propiedad { get; set; }
+
+        public Visita()
+        {
+            FechaHora = DateTime.Now;
+            AgenteInmobiliario = new AgenteInmobiliario();
+            Cliente = new Cliente();
+            Propiedad = new Propiedad();
+        }
+    }
 }
