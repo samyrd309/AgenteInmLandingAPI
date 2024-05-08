44fba2d8997e00b4cf10bf77cf00ae303702c0c0
Samuel Charles <samyrd309@gmail.com>
2024-05-08 11:17:22 -0400

    Class library entities creation adn Reference adding

--- Entities/RepositoryContext.cs
+++ Entities/RepositoryContext.cs
@@ -7,7 +7,6 @@ namespace Entities
         public RepositoryContext(DbContextOptions options) : base(options) { }
         public DbSet<TipoPropiedad> TipoPropiedades { get; set; }
         public DbSet<Propiedad> Propiedades { get; set; }
-        
         public DbSet<AgenteInmobiliario> AgentesInmobiliarios { get; set; }
         public DbSet<Cliente> Clientes { get; set; }
         public DbSet<Visita> Visitas { get; set; }
