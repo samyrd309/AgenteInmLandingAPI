a740ed7f9547c02d7c5509aa5b3b1250df842110
Samuel Charles <samyrd309@gmail.com>
2024-05-08 10:50:18 -0400

    Repository context

--- Entities/RepositoryContext.cs
+++ Entities/RepositoryContext.cs
@@ -1,1 +1,15 @@
 using Entities.Models; 
+namespace Entities
+{
+    public class RepositoryContext : DbContext
+    {
+        public RepositoryContext(DbContextOptions options) : base(options) { }
+        public DbSet<TipoPropiedad> TipoPropiedades { get; set; }
+        public DbSet<Propiedad> Propiedades { get; set; }
+        public DbSet<Propietario> Propietarios { get; set; }
+        public DbSet<Inquilino> Inquilinos { get; set; }
+        public DbSet<Contrato> Contratos { get; set; }
+        public DbSet<Pago> Pagos { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
