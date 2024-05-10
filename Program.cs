55f9ea8fb600c50226aaffe1fcf61244d8705dd9
Samuel Charles <samyrd309@gmail.com>
2024-05-10 15:06:50 -0400

    Final configuration

--- Program.cs
+++ Program.cs
@@ -1,6 +1,33 @@
+using System.Reflection;
+using Extensions;
+
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.ConfigureCors();
+builder.Services.ConfigureMySqlContext(builder.Configuration);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddEndpoints(Assembly.GetExecutingAssembly());
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapEndpoints();
+
+#region Environment
+if(app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+#endregion
+
+app.UseHttpsRedirection();
+
+
+//app.ConfigureEndpoints();
 
 app.Run();
